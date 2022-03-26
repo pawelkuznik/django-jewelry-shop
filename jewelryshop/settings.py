ec2e58cd63ac8ab339c46174fbf66676643d75da
pawelkuznik <paw.kuznik@gmail.com>
2022-03-27 00:01:42 +0100

    add hosts

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['52.58.220.246']
 
 
 # Application definition
