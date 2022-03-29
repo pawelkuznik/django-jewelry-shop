335d877afc7ec83a3bb378e424e3191e05881b98
pawelkuznik <paw.kuznik@gmail.com>
2022-03-29 07:54:20 +0200

    check pipeline

---
+++ tests/visual_regression_tests/backstop_data/html_report/config.js
@@ -0,0 +1,54 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_tests_jewelry_store_Homepage_0_document_0_normal_desktop_screen.png",
+        "test": "..\\bitmaps_test\\20220329-074143\\backstop_tests_jewelry_store_Homepage_0_document_0_normal_desktop_screen.png",
+        "selector": "document",
+        "fileName": "backstop_tests_jewelry_store_Homepage_0_document_0_normal_desktop_screen.png",
+        "label": "jewelry store Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://3.72.154.24",
+        "referenceUrl": "http://52.58.220.246",
+        "expect": 0,
+        "viewportLabel": "normal desktop screen",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\bitmaps_reference\\backstop_tests_jewelry_store_Homepage_0_document_1_common_mobile_device_screen.png",
+        "test": "..\\bitmaps_test\\20220329-074143\\backstop_tests_jewelry_store_Homepage_0_document_1_common_mobile_device_screen.png",
+        "selector": "document",
+        "fileName": "backstop_tests_jewelry_store_Homepage_0_document_1_common_mobile_device_screen.png",
+        "label": "jewelry store Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://3.72.154.24",
+        "referenceUrl": "http://52.58.220.246",
+        "expect": 0,
+        "viewportLabel": "common mobile device screen",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_tests"
+});
